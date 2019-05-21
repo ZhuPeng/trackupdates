791d3a1cc502c468c455eb5d25aaf59e93ffbe5d
Peng ZHU <zhupengbupt@gmail.com>
2019-05-21 23:49:05 +0800

    Add django admin for github recommend

--- gen_djangoadmin.sh
+++ gen_djangoadmin.sh
@@ -9,11 +9,11 @@ django-admin.py startapp trackupdates
 # replace db TODO: specified db name from yaml
 sed -i 's/db.sqlite3/..\/trackupdates.sqlite/g' djangoadmin/settings.py
 
-# auto-genenerate models.py
-python manage.py inspectdb > trackupdates/models.py 
-
 # add trackupdates to Install apps
 sed -i "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'trackupdates',/g" djangoadmin/settings.py
+
+# auto-genenerate models.py
+python manage.py inspectdb > trackupdates/models.py 
 
 # register models
 echo '
